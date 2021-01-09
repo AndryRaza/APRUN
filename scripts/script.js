845dd9dc5f5a36d6012385795549e264223e05e3
AndryRaza <70998539+AndryRaza@users.noreply.github.com>
2021-01-09 23:18:07 +0400

    Affichage des cours selon la promotion de l'apprenant

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,23 @@
 
-document.addEventListener('DOMContentLoaded', function () {
-  var calendarEl = document.getElementById('calendar');
-  var calendar = new FullCalendar.Calendar(calendarEl, {
-    initialView: 'dayGridMonth',
-    locale: 'fr'
-  });
-  calendar.render();
+  document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendar');
+
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+        headerToolbar: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+        },
+        editable: true,
+        navLinks: true, // can click day/week names to navigate views
+        dayMaxEvents: true, // allow "more" link when too many events
+        events: {
+            url: 'http://127.0.0.1:8080/edsa-App_run/includes/evenements.php'
+    
+        }
+    });
+
+    calendar.render();
 });
 
 
