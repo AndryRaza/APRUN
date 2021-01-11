bd33f2f1d816a1c35c06f422bb82d5f3e14a8b31
AndryRaza <70998539+AndryRaza@users.noreply.github.com>
2021-01-11 12:31:58 +0400

    Modification de l'accueil de l'apprenant pour le rendre "responsive"

--- pages/apprenant_edt.php
+++ pages/apprenant_edt.php
@@ -5,34 +5,35 @@ $allow = '1';
 ?>
 <?php require_once '../includes/header.php' ?>
 
-<section class="container-fluid h-100 text-center mt-5" id="edt_apprenant">
+<section class="container-fluid h-100 text-center" id="edt_apprenant">
 
-    <h1>Bonjour</h1>
+    <h1 class="mb-5">Bonjour</h1>
 
-    <h2>Emploi du temps</h2>
-    <div class="row">
-        <div class="col-4">
-            <div class="list-group" id="list-tab" role="tablist">
-                <a class="list-group-item list-group-item-action active" id="list-home-list" data-bs-toggle="list" href="#list-home" role="tab" aria-controls="home">Home</a>
-                <a class="list-group-item list-group-item-action" id="list-profile-list" data-bs-toggle="list" href="#list-profile" role="tab" aria-controls="profile">Profile</a>
+    <section class="container">
+        <div class="row row-cols-md-2 row-cols-1">
+            <div class="col-md-3">
+                <div class="list-group" id="list-tab" role="tablist">
+                    <a class="list-group-item list-group-item-action active" id="list-home-list" data-bs-toggle="list" href="#list-home" role="tab" aria-controls="home">Emploi du temps</a>
+                    <a class="list-group-item list-group-item-action" id="list-profile-list" data-bs-toggle="list" href="#list-profile" role="tab" aria-controls="profile">Stats</a>
+                </div>
             </div>
-        </div>
-        <div class="col-8">
-            <div class="tab-content" id="nav-tabContent">
-                <div class="tab-pane  show active" id="list-home" role="tabpanel" aria-labelledby="list-home-list">
-                    <div class="container" id='calendar'>
+            <div class="col-md-9">
+                <div class="tab-content" id="nav-tabContent">
+                    <div class="tab-pane  show active" id="list-home" role="tabpanel" aria-labelledby="list-home-list">
+                        <h2>Emploi du temps</h2>
+                        <div class="container" id='calendar'>
+                        </div>
                     </div>
-                </div>
-                <div class="tab-pane fade" id="list-profile" role="tabpanel" aria-labelledby="list-profile-list">
-                <?php 
-                    require_once 'apprenant_presence.php';
-                ?>
-                </div>
+                    <div class="tab-pane fade" id="list-profile" role="tabpanel" aria-labelledby="list-profile-list">
+                        <?php
+                        require_once 'apprenant_presence.php';
+                        ?>
+                    </div>
 
+                </div>
             </div>
         </div>
-    </div>
-
+    </section>
 </section>
 
 <?php
