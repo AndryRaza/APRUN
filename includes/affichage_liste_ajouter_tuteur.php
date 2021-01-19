5292be0cee886bf9740875074382a7ed3629d724
AndryRaza <70998539+AndryRaza@users.noreply.github.com>
2021-01-19 22:43:53 +0400

    Un peu + joli liste apprenant des tuteurs

--- includes/affichage_liste_ajouter_tuteur.php
+++ includes/affichage_liste_ajouter_tuteur.php
@@ -3,9 +3,11 @@
 require_once 'bdd.php';
 
 
-$req = $bdd->prepare("SELECT utilisateur.nom AS nom, utilisateur.prenom AS prenom, utilisateur.id_user as id
+$req = $bdd->prepare("SELECT utilisateur.nom AS nom, utilisateur.prenom AS prenom, utilisateur.id_user as id, promotion.nom AS promotion
                     FROM  `utilisateur_tuteur`
                     INNER JOIN `utilisateur` ON utilisateur.id_user = utilisateur_tuteur.id_user_apprenant
+                    INNER JOIN `utilisateur_promotion` on utilisateur_promotion.id_user = utilisateur.id_user 
+                    INNER JOIN `promotion` ON promotion.id_promo = utilisateur_promotion.id_promo
                     WHERE id_user_tuteur IS NULL
 ");
 $req->execute();
@@ -16,14 +18,16 @@ $req->closeCursor();
 
 <section class="container-fluid py-5">
     <div class="container">
+        <h1 class="text-center">Liste des apprenants sans tuteur</h1>
         <ul class="list-group list-group-flush text-center">
             <?php foreach ($tab as $key => $value) { ?>
-                <li class="list-group-item d-flex"><?= $value['nom'] . ' ' . $value['prenom']; ?>
+                <li class="list-group-item d-flex justify-content-center"> Nom : <?= $value['nom'] . '<br> Prénom : ' . $value['prenom']
+                                                                . '<br> Promotion : ' . $value['promotion']; ?>
                     <form action="../includes/liaison_tuteur_apprenant.php" method="POST">
                         <input type="hidden" name="id_tuteur" value="<?= $id_tuteur ?>">
                         <input type="hidden" name="id_apprenant" value="<?= $value['id'] ?>">
-                        <button class="btn" type="submit" name="add">
-                            <img src="../ressources/img/add.png" width="40px" height="40px">
+                        <button class="btn mt-3" type="submit" name="add">
+                            <img src="../ressources/img/add.png" width="20px" height="20px">
                         </button>
                     </form>
                 </li>
--- includes/liste_apprenant_tuteur.php
+++ includes/liste_apprenant_tuteur.php
@@ -5,9 +5,11 @@ $id_tuteur = $_POST['id'];
 $nom_tuteur = $_POST['nom_tuteur'];
 
 
-$req = $bdd->prepare("SELECT utilisateur.nom AS nom, utilisateur.prenom AS prenom
+$req = $bdd->prepare("SELECT utilisateur.nom AS nom, utilisateur.prenom AS prenom, promotion.nom AS promotion
                  FROM  `utilisateur_tuteur`
                  INNER JOIN `utilisateur` ON utilisateur.id_user = utilisateur_tuteur.id_user_apprenant
+                 INNER JOIN `utilisateur_promotion` on utilisateur_promotion.id_user = utilisateur.id_user 
+                 INNER JOIN `promotion` ON promotion.id_promo = utilisateur_promotion.id_promo
                  WHERE id_user_tuteur = '$id_tuteur'
     
     ");
@@ -22,14 +24,16 @@ $req->closeCursor();
         <div class="d-flex justify-content-center">
             <form action="admin_ajouter_apprenants_tuteur.php" method="POST">
                 <input type="hidden" name="id_tuteur" value="<?= $id_tuteur ?>">
-                <button class="btn" type="submit" name="add">
+                <button class="btn mb-3" type="submit" name="add">
                     <img src="../ressources/img/add.png" width="40px" height="40px">
                 </button>
             </form>
         </div>
         <ul class="list-group list-group-flush text-center">
             <?php foreach ($tab as $key => $value) { ?>
-                <li class="list-group-item"><?= $value['nom'] . ' ' . $value['prenom']; ?></li>
+                <li class="list-group-item">Nom : <?= $value['nom'] . '<br> Prénom : ' . $value['prenom'] 
+                 . '<br> Promotion : ' . $value['promotion']
+                 ; ?></li>
             <?php } ?>
         </ul>
     </div>
